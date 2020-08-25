dea9d4183822256d0b3e77af14a4f2ec961072b4
yxgeee <geyixiao831@gmail.com>
2020-08-25 10:27:01 +0800

    fix load

--- hubconf.py
+++ hubconf.py
@@ -7,5 +7,5 @@ def vgg16_netvlad(pretrained=False):
     pool_layer = models.create('netvlad', dim=base_model.feature_dim)
     model = models.create('embednetpca', base_model, pool_layer)
     if pretrained:
-        model.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/yxgeee/OpenIBL/releases/download/v0.1.0-beta/vgg16_netvlad.pth'))
+        model.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/yxgeee/OpenIBL/releases/download/v0.1.0-beta/vgg16_netvlad.pth', map_location=torch.device('cpu')))
     return model
